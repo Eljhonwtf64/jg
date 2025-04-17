a5a964b5092a8256d2bd2bead110f556177a77f3
Eljhonwtf64 <jhonguerra904@gmail.com>
2025-04-17 17:27:14 -0400

    Actualizar responderBot.js

--- plugins/responderBot.js
+++
@@ -1,6 +0,0 @@
-export default async function (m, { conn }) {
-         if (m.text && m.text.toLowerCase().includes('bot')) {
-             const replyMessage = '¡Hola! ¿Me llamaste? 😊';
-             await conn.sendMessage(m.chat, { text: replyMessage }, { quoted: m });
-         }
-     }
